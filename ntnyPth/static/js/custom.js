790358438a23f04abeeb61c8b35d3d6f0c521e57
matthewli125 <37080094+matthewli125@users.noreply.github.com>
2020-04-27 20:25:32 -0400

    Add files via upload

---
+++ ntnyPth/static/js/custom.js
@@ -0,0 +1,39 @@
+(function($) {
+
+  // Navigation scrolls
+  $('.navbar-nav li a').bind('click', function(event) {
+    $('.navbar-nav li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    var $anchor = $(this);
+    var nav = $($anchor.attr('href'));
+    if (nav.length) {
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 78
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    }
+  });
+
+  $(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse.collapse").removeClass('in');
+  });
+
+  // Porfolio filer
+  $("#portfolio-flters li").click(function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    var selectedFilter = $(this).data("filter");
+    $("#portfolio-wrapper").fadeTo(100, 50);
+
+    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+
+    setTimeout(function() {
+      $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+      $("#portfolio-wrapper").fadeTo(300, 1);
+    }, 300);
+  });
+
+
+})(jQuery);
